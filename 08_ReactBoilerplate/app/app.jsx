306ad5e763b31f302c53d063c8fa40cff191c8de
shail121 <shail1312@hotmail.com>
2017-04-24 23:19:43 +0530

    boilerplate

--- 08_ReactBoilerplate/app/app.jsx
+++ 08_ReactBoilerplate/app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Shailesh',
+  location: 'India'
+};
+
+var objTwo = {
+  age:25,
+  ...objOne
+};
+
+console.log(objTwo);
+
 ReactDOM.render(
   <h1>This is boilerplate</h1>,
   document.getElementById('app')
