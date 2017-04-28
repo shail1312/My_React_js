d9a8c0fe0d25368cba6f5bfa9b7ea992d0e88282
shail121 <shail1312@hotmail.com>
2017-04-28 12:39:40 +0530

    heroku

--- 09_Weather_project/server.js
+++ 09_Weather_project/server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function (req,resp,next) {
+  if(req.headers['x-forworded-proto'] == 'http'){
+    next();
+  } else {
+    resp.redirect('http://'+req.hostname+req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000,function () {
-    console.log('Express server is up on port 3000');
+app.listen(port,function () {
+    console.log('Express server is up on port ' + port);
 });
