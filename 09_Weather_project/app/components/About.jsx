1f5672b7663cade768299dca745b9e8ed60b9831
shail121 <shail1312@hotmail.com>
2017-04-30 11:54:32 +0530

    React Boilerplate

--- 09_Weather_project/app/components/About.jsx
+++ 09_Weather_project/app/components/About.jsx
@@ -10,7 +10,15 @@ var React = require('react');
 
 var About = (props) => {
     return (
-      <h3>About Component</h3>
+      <div >
+        <h1 className="text-center">About</h1>
+        <p>This is my first React application. I develop this application for learning React.</p>
+        <p>Here are some tools i use.</p>
+        <ul>
+          <li>React.</li>
+          <li>Open weather maps.</li>
+        </ul>
+      </div>
     );
   };
 
--- 09_Weather_project/app/components/Examples.jsx
+++ 09_Weather_project/app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) =>{
   return (
-    <h3>Examples Component</h3>
-  );
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example to try out!</p>
+      <ol>
+        <li>
+          <Link to='/?location=Raipur'>Raipur, CG</Link>
+        </li>
+        <li>
+          <Link to='/?location=Durg'>Durg, CG</Link>
+        </li>
+      </ol>
+    </div>
+  )
 };
 
 module.exports = Examples;
